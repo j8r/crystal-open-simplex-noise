a2f0630bf3b32fbdde4d041e22c5f1a150c4a9f1
Julien Reichardt <git@jrei.ch>
2020-04-15 18:44:43 +0200

    Use UInt64 to prevent overflows

--- src/open-simplex-noise.cr
+++ src/open-simplex-noise.cr
@@ -4,6 +4,9 @@ require "./4d"
 
 struct OpenSimplexNoise
   def initialize(seed : Int64 = 0_i64)
+    # Prevents overflows
+    seed = seed.to_u64
+  
     # Initiate the class and generate permutation arrays from a seed number.
 
     # Initializes the class using a permutation array generated from a 64-bit seed.
@@ -13,15 +16,19 @@ struct OpenSimplexNoise
     @perm_grad_index_3d = Array(Int32).new(256, 0)
 
     source = (0...256).to_a
-    3.times { seed = seed &* 6364136223846793005 &+ 1442695040888963407 }
+    3.times { seed = new_seed seed }
 
     source.reverse.each do |i|
-      seed = seed &* 6364136223846793005 &+ 1442695040888963407
+      seed = new_seed seed
       r = (seed + 31) % (i + 1)
       r += i + 1 if r < 0
       @perm[i] = source[r]
       @perm_grad_index_3d[i] = (@perm[i] % (GRADIENTS_3D.size // 3)) * 3
       source[r] = source[i]
     end
+  end
+
+  private def new_seed(seed : UInt64) : UInt64
+    seed &* 6364136223846793005 &+ 1442695040888963407
   end
 end
